bb9fa9ff615a21ce1de451a18ee58ce1448d4cb1
James Graham <J.A.Graham@soton.ac.uk>
2015-11-09 12:22:19 +0000

    Implemented bonded terms (currently only bond-length-harmonic) in similar style to LAMMPS allowing easy addition of other functional forms;

---
+++ include/bond.h
@@ -0,0 +1,22 @@
+//
+// Created by james on 09/11/15.
+//
+
+#ifndef MINIMD_BOND_H
+#define MINIMD_BOND_H
+
+#include "globals.h"
+
+class Bond{
+protected:
+    const double equilibrium_, forceConstant_;
+    Bond(const double eqm, const double fc) : equilibrium_(eqm), forceConstant_(fc){};
+public:
+    virtual void calcForces(MyTypes::vecList &x, MyTypes::vecList &f) const = 0;
+
+//    Bond(const Bond &other) = delete;
+//    Bond &operator=(const Bond &other) = delete;
+};
+
+
+#endif //MINIMD_BOND_H
---
+++ src/bond-length-harmonic.cpp
@@ -0,0 +1,32 @@
+//
+// Created by james on 09/11/15.
+//
+
+#include "bond-length-harmonic.h"
+
+#include <cmath>
+
+void BondLengthHarmonic::calcForces(MyTypes::vecList &x, MyTypes::vecList &f) const{
+    MyTypes::vec del;
+    del[0] = x[atoms_[0]][0] - x[atoms_[1]][0];
+    del[1] = x[atoms_[0]][1] - x[atoms_[1]][1];
+    del[2] = x[atoms_[0]][2] - x[atoms_[1]][2];
+
+    const double r = std::sqrt(del[0]*del[0] + del[1]*del[1] + del[2]*del[2]);
+    const double dr = r - equilibrium_;
+    const double rk = forceConstant_ * dr;
+
+    double fbond;
+    if(r > 0.){
+        fbond = -2. * rk / r;
+    }else{
+        fbond = 0.;
+    }
+
+    f[atoms_[0]][0] += del[0] * fbond;
+    f[atoms_[0]][1] += del[1] * fbond;
+    f[atoms_[0]][2] += del[2] * fbond;
+    f[atoms_[1]][0] -= del[0] * fbond;
+    f[atoms_[1]][1] -= del[1] * fbond;
+    f[atoms_[1]][2] -= del[2] * fbond;
+}
